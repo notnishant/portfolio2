0d16e4ea6e758dbba1ada9b09a718128477b7703
pawan <pwnkumar0099@gmail.com>
2021-05-29 23:28:49 +0530

    drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:google_fonts/google_fonts.dart';
+import 'screens/homepage.dart';
 
 void main() => runApp(MyApp());
-
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          toolbarHeight: 70,
-          centerTitle: true,
-          title: SizedBox(
-            child: Image.asset('images/Portfoliologo.png',
-            ),
-          ),
-          backgroundColor: Colors.white,
-          iconTheme: IconThemeData(color: Colors.black),
-          elevation: 0,
-        ),
-        body:
-        Column(
-          //crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-        Container(
-          padding: EdgeInsets.fromLTRB(42, 50, 0, 10),
-          child: Row(
-          children: [
-          Container(
-          width: 45,
-            height: 1,
-            color: Color(0xff7868e6),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
-            child: Text(
-              "About",
-              style: TextStyle(fontSize: 17,fontWeight: FontWeight.w300),
-            ),
-          ),
-          ],
-      ),
-        ),
-      Container(
-        padding: EdgeInsets.fromLTRB(0, 30, 0, 50),
-        child: Align(
-          alignment: Alignment(-0.5, 0),
-          child: Text(
-            "Hello, my \nname's Nishant. \nI’m a Flutter\nDeveloper.",
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ),Center(
-          child: Image.asset(
-            'images/imagevector.png',
-          ),
-        ),
-            Container(
-              padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
-              child: Text(
-                'Swipe up',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 15),
-              ),
-            ),
-            Image.asset(
-              'images/Vectorarrow.jpg',
-              width: 15,
-              height: 15,
-            ),
-          ],
-        ),
-        drawer: Drawer(),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/screens/homepage.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:portfolio2/screens/Drawer.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.poppinsTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          toolbarHeight: 80,
+          centerTitle: true,
+          title: SizedBox(
+            //child: Image.asset('images/Portfoliologo.png',
+            child: RichText(
+              text: TextSpan(
+                style: TextStyle(color: Colors.black, fontSize: 24),
+                children: <TextSpan>[
+                  TextSpan(text: 'Port', style: TextStyle(fontWeight: FontWeight.w900 )),
+                  TextSpan(text: 'folio'),
+                ],
+              ),
+            ),
+          ),
+          backgroundColor: Colors.white,
+          iconTheme: IconThemeData(color: Colors.black),
+          elevation: 0,
+        ),
+        body:
+        Column(
+          //crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.fromLTRB(42, 40, 0, 10),
+              child: Row(
+                children: [
+                  Container(
+                    width: 45,
+                    height: 1,
+                    color: Color(0xff7868E6),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(8, 0, 0, 0),
+                    child: Text(
+                      "About",
+                      style: TextStyle(fontSize: 15,fontWeight: FontWeight.w500,color: Color(0xff7868E6)),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 15, 0, 30),
+              child: Align(
+                alignment: Alignment(-0.5, 0),
+                child: Text(
+                  "Hello, my \nname's Nishant. \nI’m a Flutter\nDeveloper.",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ),
+            ),Center(
+              child: Image.asset(
+                'images/imagevector.png',
+                //'images/8401.jpg',
+                //fit: BoxFit.scaleDown,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 5),
+              child: Text(
+                'Swipe up',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            Image.asset(
+              'images/Vectorarrow.jpg',
+              width: 15,
+              height: 15,
+            ),
+          ],
+        ),
+        drawer: Container(
+          width: 500,
+          child: Drawer(
+            child: Draw(),
+          ),
+        ),
+      ),
+    );
+  }
+}
